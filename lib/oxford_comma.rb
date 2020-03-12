233649de3e62a60ab8a81ff1024eeff0a4fcf128
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-12 03:11:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
-def oxford_comma (array)
-  if array.size == 1 
-    return array [0]
-  elsif array.size == 2 
-   return array.join (" and ")
-  else 
-    return array[0..-2].join(', ') + ",
+def oxford_comma(array)
+  if array.length == 2
+    array [-2] << " and "
+    array.join 
+    elsif array.length == 1 
+      array.join
+    elsif array.length > 2 
+      array [-1].prepend "and "
+    array.join{", ")
   end 
-end 
+end  
+    
